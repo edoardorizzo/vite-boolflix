112f3c7ad609d81b2fd32f7dfc01de29c49b1911
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-19 13:37:10 +0200

    print on screen film list

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,21 @@ export default {
     }
   },
   methods: {
-
+    searchMovies(){
+      console.log(this.state.searchText);
+      const url = this.state.URL_API + `?name=${this.state.searchText}`
+      this.state.callApi(url)
+    }
   }
 }
 </script>
     
 <template>
   <div>
-    <input @keyup.enter="searchMovies" type="text" v-model="state.searchText" placeholder="Search movie title...">
+    <input type="text" v-model="state.searchText" placeholder="Search movie title...">
     <button @click="searchMovies">Search</button>
     <ul>
-      <li v-for="movie in state.movies" :key="movie.id">
+      <li v-for="movie in state.movies">
         <h4>{{ movie.title }}</h4>
         <p>{{ movie.original_title }}</p>
         <p>{{ movie.original_language }}</p>
--- src/state.js
+++ src/state.js
@@ -3,16 +3,18 @@ import { reactive } from 'vue';
 
 export const state = reactive({
     searchText: '',
-    movies: [],
-    URL_API: 'https://api.themoviedb.org/3/search/movie?api_key=d94700216696537359e360c15ede3654&query=matrix',
+    movies: ['searchMovies'],
+    URL_API: 'https://api.themoviedb.org/3/search/movie?api_key=d94700216696537359e360c15ede3654&query=m',
     callApi(url) {
         axios
-          .get(url)
-          .then((response) => {
-            console.lo(response);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
+            .get(url)
+            .then((response) => {
+                console.log(response);
+                this.movies = response.data.results
+                this.info = response.data.info
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
 }) 
