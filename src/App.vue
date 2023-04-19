9f87b5357627a3eacb1c8e8ae37253e2807a44f0
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-19 17:01:31 +0200

    refactoring app.vue

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,7 @@ export default {
 
       }
       return languageMap[language] || 'Sconosciuto'
-
-    }
+    },
   },
 }
 </script>
@@ -35,14 +34,15 @@ export default {
     <button @click="searchMovies">Search</button>
     <ul>
       <li v-for="movie in state.movies">
-        <!-- if condition for title or name -->
+        
         <h4 v-if="movie.title">{{ movie.title }}</h4>
         <h4 v-else>{{ movie.name }}</h4>
         
         <img :src="`${state.coverUrl}${movie.poster_path}`" alt="">
+
         <p v-if="movie.original_title">{{ movie.original_title }}</p>
         <p>{{ getLanguageName(movie.original_language) }}</p>
-        <p>{{ movie.vote_average }}</p>
+        <p class="vote_movie">{{ movie.vote_average }}</p>
       </li>
     </ul>
   </div>
