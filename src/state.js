46e7e2f3e2b48c4d16d3027cbd04a63cccc5b7c4
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-19 13:39:03 +0200

    refactoring typo statejs

--- src/state.js
+++ src/state.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue';
 
 export const state = reactive({
     searchText: '',
-    movies: ['searchMovies'],
+    movies: [],
     URL_API: 'https://api.themoviedb.org/3/search/movie?api_key=d94700216696537359e360c15ede3654&query=m',
     callApi(url) {
         axios
