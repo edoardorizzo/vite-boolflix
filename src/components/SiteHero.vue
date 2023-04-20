67c42805f3fed8f1356e29c89d11f240944139dd
edoardorizzo <edo96.rizzo@gmail.com>
2023-04-20 11:47:20 +0200

    add sitehero component

--- src/components/SiteHero.vue
+++ src/components/SiteHero.vue
@@ -8,18 +8,75 @@ export default {
         }
     },
     methods: {
-        
+
     },
 
 }
 </script>
 
 <template>
-    <input @keyup.enter="state.searchMovies" type="text" v-model="state.searchText" placeholder="Insert movie o Tv Show...">
-    <button @click="state.searchMovies">Search</button>
+    <div class="hero">
+        <div class="hero_elements">
+            <h1>Scopri un mondo fantastico!</h1>
+            <p>Goditi i migliori <strong>Film</strong> e <strong>Serie Tv</strong> grazie al servizio <strong>BoolFlix</strong>, servizio streaming <strong>completamente gratuito</strong>  ed indipendente che nulla ha a che fare con un noto servizio di streaming con un nome simile ma non troppo!</p>
+            <div class="input_container">
+                <input @keyup.enter="state.searchMovies" type="text" v-model="state.searchText"
+                    placeholder="Insert movie o Tv Show...">
+                <button @click="state.searchMovies">Search</button>
+            </div>
+        </div>
+
+
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
 @use '../assets/partials/partials.scss' as *;
+
+.hero {
+    background-image: url(/img/hero_bg.png);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 400px;
+    padding: 8rem 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        color: $white;
+        margin-bottom: 1rem;
+    }
+
+    p{
+        color: $white;
+        font-size: 14px;
+        margin-bottom: 2rem;
+    }
+
+    .input_container {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        max-width: 600px;
+    }
+
+    input {
+        border-radius: 1rem;
+        border: 2px solid $dark-blue;
+        width: 240px;
+        padding: 0.25rem;
+    }
+
+    button {
+        border-radius: 1rem;
+        border: none;
+        background-color: $green;
+        width: 120px;
+        color: $white;
+        padding: 0.25rem;
+    }
+}
 </style>
